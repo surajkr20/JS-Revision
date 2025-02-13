821dccd654cf09bb3f2e4e3cb9bc82d3c06f5294
surajkr20 <suraj71308kumar@gmail.com>
2025-02-13 11:13:27 +0530

    callback fun

--- AsyncProgramming/callback.js
+++ AsyncProgramming/callback.js
@@ -1,11 +1,37 @@
 
-// calling a function inside a fun
 
-let outer = () =>{
-    return inner = () =>{
-        console.log("hello")
-    }
+// calling a function inside a fun.
+// =>> javascript runs top to the bottom
+
+// function one(call_two){
+//     console.log('step 1 completed, please call step 2');
+//     call_two();
+// }
+
+// function two(){
+//     console.log('step 2')
+// }
+
+// one(two);
+
+// another example
+
+// let order = (call_production) =>{
+//     console.log("order placed, please call production");
+//     call_production();
+// }
+
+// let production = () =>{
+//     console.log("order received, starting production")
+// }
+
+// order(production);
+
+// how to access
+
+let stocks = {
+    Fruits : ["strawberry", "grapes", "banana", "apple"]
 }
 
-let innerResult = outer();
-innerResult()
+console.log(stocks.Fruits[2])
+console.log(stocks.Fruits)
